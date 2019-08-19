b301fbc49be8fa9c5fb8db9eda3fe8384f157c6c
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-08-19 22:54:58 +0300

    hw-02

--- src/routes/route.js
+++
@@ -1,8 +0,0 @@
-const productRouter = require('./Product-router');
-const userRouter = require('./user-router')
-
-const router = {
-    '/products': productRouter,
-    '/signup': userRouter
-}
-module.exports = router;
